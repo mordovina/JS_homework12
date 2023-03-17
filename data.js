490f4418a9f308d6781a55a2aa3093057d8f69be
yuliya mordovina <mordovina_y@list.ru>
2023-03-17 22:43:47 +0300

    homework 12

---
+++ data.js
@@ -0,0 +1,18 @@
+const dataInfo = `[
+    {
+        "name": "MANGO  PEOPLE  T-SHIRT",
+        "price": "300",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "2" ,
+        "img": "img/1_img.jpg"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "52",
+        "color": "Black",
+        "size": "XL",
+        "quantity": "1",
+        "img": "img/2_img.jpg"
+    }
+]`
